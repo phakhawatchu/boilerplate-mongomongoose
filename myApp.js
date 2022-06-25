def0857ff9f597d8aac43e87d858d01b2ea80924
Phakhawat Chullamonthon <phakhawat.chu@outlook.com>
2022-06-25 20:45:18 +0700

    feat: add queryChain

--- myApp.js
+++ myApp.js
@@ -95,8 +95,14 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
     const foodToSearch = "burrito";
-
-    done(null /*, data*/);
+    Person.find({ favoriteFoods: foodToSearch })
+        .sort({ name: 1 })
+        .limit(2)
+        .select({ age: 0 })
+        .exec(function (err, data) {
+            if (err) return console.error(err);
+            else done(null, data);
+        });
 };
 
 /** **Well Done !!**
