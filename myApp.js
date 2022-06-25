a93730263619e034b649ad66e43d035ca1f73d27
Phakhawat Chullamonthon <phakhawat.chu@outlook.com>
2022-06-25 19:53:47 +0700

    feat: add createManyPeople

--- myApp.js
+++ myApp.js
@@ -31,7 +31,10 @@ const createAndSavePerson = (done) => {
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-    done(null /*, data*/);
+    Person.create(arrayOfPeople, (err, data) => {
+        if (err) return console.error(err);
+        else done(null, data);
+    });
 };
 
 const findPeopleByName = (personName, done) => {
