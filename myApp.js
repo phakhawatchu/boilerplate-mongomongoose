c6cc9a4f011d10607b9040d34756a353a7ae9c7e
Phakhawat Chullamonthon <phakhawat.chu@outlook.com>
2022-06-25 20:40:13 +0700

    feat: add findAndUpdate

--- myApp.js
+++ myApp.js
@@ -72,8 +72,10 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
     const ageToSet = 20;
-
-    done(null /*, data*/);
+    Person.findOneAndUpdate({ name: personName }, { age: ageToSet }, { new: true }, (err, data) => {
+        if (err) return console.error(err);
+        else done(null, data);
+    });
 };
 
 const removeById = (personId, done) => {
