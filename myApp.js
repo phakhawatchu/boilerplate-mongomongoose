b25220b8e2955ec137e0406d9731ab760d5d76e5
Phakhawat Chullamonthon <phakhawat.chu@outlook.com>
2022-06-25 20:42:55 +0700

    feat: add removeManyPeople

--- myApp.js
+++ myApp.js
@@ -87,8 +87,10 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
     const nameToRemove = "Mary";
-
-    done(null /*, data*/);
+    Person.remove({ name: nameToRemove }, (err, data) => {
+        if (err) return console.error(err);
+        else done(null, data);
+    });
 };
 
 const queryChain = (done) => {
